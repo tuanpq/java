73289d613e8afc62288133ff1e98304cad23c9c9
tuanpq <tuanpq@gmail.com>
2025-03-06 10:02:00 +0700

    Add capture screen and save to Excel function

---
+++ selenium/README.md
@@ -0,0 +1,2 @@
+# Selenium
+Run automation test, capture screen and save to Excel file.
--- selenium/src/main/java/io/github/apitrainer/selenium/App.java
+++ selenium/src/main/java/io/github/apitrainer/selenium/App.java
@@ -1,8 +1,18 @@
-package io.github.apitraining.selenium;
+package io.github.apitrainer.selenium;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
+import org.apache.poi.xssf.usermodel.XSSFDrawing;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -35,6 +45,31 @@ public class App {
 			File target = new File("./screenshot.png");
 			Files.copy(source, target);
 			System.out.println("Captured file: " + target.getAbsolutePath());
+			
+			Workbook workbook = new XSSFWorkbook();
+			Sheet sheet = workbook.createSheet("UT01-001");
+			Row row0 = sheet.createRow(0);
+			Row row1 = sheet.createRow(1);
+			Row row2 = sheet.createRow(2);
+			row0.createCell(0).setCellValue("Pre-condition");
+			row1.createCell(1).setCellValue("User's role is Developer");
+			row2.createCell(1).setCellValue("Navigate from Product List screen");
+			InputStream targetStream = new FileInputStream(target.getAbsolutePath());
+			byte[] bytes = IOUtils.toByteArray(targetStream);
+			int pictureIdx = workbook.addPicture(bytes, Workbook.PICTURE_TYPE_PNG);
+			targetStream.close();
+			XSSFDrawing drawing = (XSSFDrawing) sheet.createDrawingPatriarch();
+			XSSFClientAnchor anchor = new XSSFClientAnchor();
+			anchor.setCol1(1);
+			anchor.setRow1(4);
+			anchor.setCol2(26);
+			anchor.setRow2(44);
+			drawing.createPicture(anchor, pictureIdx);
+			try (FileOutputStream fileOut = new FileOutputStream("./Evidence.xlsx")) {
+				workbook.write(fileOut);
+				workbook.close();
+			}
+			
 			Thread.sleep(5000);
 		} catch (IOException e) {
 			e.printStackTrace();
